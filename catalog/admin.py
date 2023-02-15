9c6eb117bc479396b795711aed6a95e16b01a897
Thomas Davies <tdavies7@sheffield.ac.uk>
2023-02-15 11:06:34 +0000

    fuck2

--- catalog/admin.py
+++ catalog/admin.py
@@ -7,8 +7,10 @@ admin.site.register(Language)
 admin.site.register(Genre)
 # admin.site.register(BookInstance)
 
+
 class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    list_display = ('last_name', 'first_name', 
+                    'date_of_birth', 'date_of_death')
 
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
 # Register the Admin classes for Book using the decorator
