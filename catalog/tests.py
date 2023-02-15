f8e6503c36bd6eec687c44c6b9c76e3c88848763
Thomas Davies <tdavies7@sheffield.ac.uk>
2023-02-15 10:40:26 +0000

    nc

--- catalog/tests.py
+++ catalog/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
-
+from .models import Genre
 # Create your tests here.
+class ModelTesting(TestCase):
+
+    def setUp(self):
+        self.catalog = Genre.objects.create(name="Science fiction")
+
+    def test_genre_model(self):
+        d = self.catalog
+        self.assertTrue(isinstance(d, Genre))
